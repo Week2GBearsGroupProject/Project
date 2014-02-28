0f3d01c98163eba9643b73bfc2effe0eeb8af3f5
Apprentice <apprentice@dbc29.local>
2014-02-28 15:53:36 -0800

    Updates to QuizProgram.run to achieve MVP

--- master_outline.rb
+++ master_outline.rb
@@ -1,24 +1,24 @@
-User will turn on program
-Program will welcome user "Welcome to DBC Quiz Factory!"
-# User will fetch a quiz
-#Program will announce the beginning of the quiz "Quiz beginning"
+# User will turn on program
+# Program will welcome user "Welcome to DBC Quiz Factory!"
+# # User will fetch a quiz
+# #Program will announce the beginning of the quiz "Quiz beginning"
 
-game loop
-  Console will display a question
-  User inputs guess into console
-    - if user_guess == quiz_answer then move on to next question
-      else the console tell them to try again.
-      user inputs new guess
-      user can input an escape word to move to next question
- end loop when all questions answered
+# game loop
+#   Console will display a question
+#   User inputs guess into console
+#     - if user_guess == quiz_answer then move on to next question
+#       else the console tell them to try again.
+#       user inputs new guess
+#       user can input an escape word to move to next question
+#  end loop when all questions answered
 
- display score
+#  display score
 
- score a tally of all questions answered correctly
+#  score a tally of all questions answered correctly
 
-# class DataAdapter
-#   convert_txt_to_quizinit(filepath)
-# end
+# # class DataAdapter
+# #   convert_txt_to_quizinit(filepath)
+# # end
 
 class Quiz
 def initialize
@@ -40,7 +40,7 @@ class Question
 #be able to call question and answer
 #if using a hash then question refers to the key and answer value
 #returns strings of question/answer
-  def initialize(options{})
+  def initialize(options = {})
     @question = options[:question]  #-> these are strings
     @answer = options[:answer]
   end
@@ -49,15 +49,35 @@ end
 
 
 class QuizProgram
+  def self.run
+    Display.welcome_screen
+    quiz1 = Quiz.new
+    @score = 0
+    gets.chomp  #Wait for user to hit enter
 
-  def run
     #main program loop is in here
-    #Tell Display to show welcome
-    #Wait for user to pr
+    while true
+
+      #Get next question from Quiz, save to a temp_var
+      cur_question = quiz1.next_question
+
+      #Tell display to show user the question
+      Display.show_question(cur_question)
+
+      #Get user input
+      user_answer = gets.chomp
+
+      #Compare user input to the current_q's answer
+      #conditional logic to go to next approrpirate step (next question or incorrect screen)
+      if user_answer == cur_question.answer
+        Display.show_correct_answer
+        @score += 1
+      else
+        Display.show_wrong_answer
+      end
+    end
+    Display.summary_screen(@score, "ERROR")
   end
-
-
-
 end
 
 class Display
